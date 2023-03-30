cd3ed5353ac2d0ebd52e1d30ddf70ce4894e9f6f
Libardo Arroyave <laastos@gmail.com>
2023-03-29 21:11:57 -0500

    WIP

--- mokepon/index.js
+++ mokepon/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+
+app.use(express.static('public'))
 // Gestion de CORS
 app.use(cors())
 // Habilita la gestión de JSON en las peticiones
